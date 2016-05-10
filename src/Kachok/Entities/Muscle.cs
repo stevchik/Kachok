4c3544e8173a513f1d2332bb520052ef57e44258
stevchik <stevchik@yahoo.com>
2016-05-09 22:38:41 -0500

    First Checking

---
+++ src/Kachok/Entities/Muscle.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kachok.Entities
+{
+    public class Muscle
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
